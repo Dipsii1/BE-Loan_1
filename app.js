54a8b411ad0b9f7eee04ca7c196768054fa79a8d
Dipsii1 <nadifaryastia@gmail.com>
2025-12-31 13:24:41 +0700

    connect for fe domain

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://frontend-kamu.vercel.app"
+      "https://loan-mu-nine.vercel.app"
     ],
     credentials: true,
   })
